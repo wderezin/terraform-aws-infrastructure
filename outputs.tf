afb84c901c9568277280b45bc8c5d550d8ccca02
Walter Derezinski <walter@daringway.com>
2020-04-12 21:35:20 -0400

    updaed provider information

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,10 @@ output zone_id {
   value = aws_route53_zone.default.zone_id
 }
 
+output tags {
+  value = local.tags
+}
+
 // We define this as a local here so it can be used in the submodule secondary-region in main.tf
 locals {
   account_info = {

cbbb61d3c124b239c5e32368c54512cbaf20459d
Walter Derezinski <walter@daringway.com>
2020-05-21 21:48:29 -0400

    disabled dynamic-route52

--- main-acm.tf
+++ main-acm.tf
@@ -12,7 +12,7 @@ resource "aws_acm_certificate" "cert" {
 resource "aws_route53_record" "cert_validation" {
   name    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_name
   type    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_type
-  zone_id =  aws_route53_zone.default.zone_id
+  zone_id = aws_route53_zone.default.zone_id
   records = [aws_acm_certificate.cert.domain_validation_options.0.resource_record_value]
   ttl     = 60
   lifecycle {

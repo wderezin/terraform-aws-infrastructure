7a5fab231a55301d0df9aeade4a39abadea74081
Walter Derezinski <walter@daringway.com>
2020-08-02 11:10:57 -0400

    moved cloudtrail to module

--- main-modules.tf
+++ main-modules.tf
@@ -3,4 +3,5 @@ module cloudtrail {
   source = "./modules/cloudtrail"
 
   cloudtrail_cloudtrail_retention_days = local.cloudtrail_cloudtrail_retention_days
+  tags                                 = local.tags
 }

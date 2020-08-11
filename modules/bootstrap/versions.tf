9e97baf3bf671494c04d3c73e9d2586df0afa78c
Walter Derezinski <walter@daringway.com>
2020-08-11 16:15:12 -0400

    removed version requirements

--- modules/bootstrap/versions.tf
+++ modules/bootstrap/versions.tf
@@ -1,9 +1,9 @@
 
-terraform {
-  required_version = "~> 0.13.0"
-  required_providers {
-    aws = "~> 3.0"
-  }
-}
+//terraform {
+//  required_version = "~> 0.13.0"
+//  required_providers {
+//    aws = "~> 3.0"
+//  }
+//}
 
 data aws_caller_identity current {}

cb21a68dc2ca6d66db8510660f54b81be40c96ca
Walter Derezinski <walter@daringway.com>
2020-09-04 23:30:48 -0400

    formating

--- locals.tf
+++ locals.tf
@@ -10,10 +10,10 @@ locals {
   )
 
   //  ecount variables make it easy to specify count for resources/modules
-  ec2_ecount                           = var.ec2_enabled ? 1 : 0
-  log_bucket_ecount                    = var.log_bucket_enabled ? 1 : 0
-  cloudtrail_ecount                    = var.cloudtrail_enabled && var.log_bucket_enabled ? 1 : 0
-  managed_backups_ecount               = var.managed_backups_enabled ? 1 : 0
+  ec2_ecount                    = var.ec2_enabled ? 1 : 0
+  log_bucket_ecount             = var.log_bucket_enabled ? 1 : 0
+  cloudtrail_ecount             = var.cloudtrail_enabled && var.log_bucket_enabled ? 1 : 0
+  managed_backups_ecount        = var.managed_backups_enabled ? 1 : 0
   default_security_group_ecount = var.default_security_group_managed ? 1 : 0
 
   force_destroy = var.force_destroy
